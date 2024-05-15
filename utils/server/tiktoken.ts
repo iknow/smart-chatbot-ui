7fbd177435312f73c1e38cfc9b13826be4acf0fd
Bearice Ren <bearice@icybear.net>
2024-05-15 10:06:54 +0900

    update tiktoken for gpt-4o

--- utils/server/tiktoken.ts
+++ utils/server/tiktoken.ts
@@ -1,13 +1,16 @@
 import cl100k from 'tiktoken/encoders/cl100k_base.json';
 import p50k from 'tiktoken/encoders/p50k_base.json';
 import { Tiktoken } from 'tiktoken/lite';
-const { encoding_for_model } = require('tiktoken');
+import { encoding_for_model } from 'tiktoken';
 
 export const getTiktokenEncoding = async (model: string): Promise<Tiktoken> => {
   // Azure fix
   const modelId = model.replace('gpt-35', 'gpt-3.5')
   if (modelId.indexOf('text-davinci-') !== -1) {
     return new Tiktoken(p50k.bpe_ranks, p50k.special_tokens, p50k.pat_str);
+  }
+  if (modelId.indexOf('gpt-4o') !== -1) {
+    return encoding_for_model('gpt-4o');
   }
   if (modelId.indexOf('gpt-3.5') !== -1) {
     return encoding_for_model('gpt-3.5-turbo', {
