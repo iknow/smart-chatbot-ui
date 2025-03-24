93f2de51439369734955cf956af5acaf72ce5a4f
Bearice Ren <bearice@icybear.net>
2025-03-24 15:13:26 +0900

    fixes infinite redirects by disabling i18n

--- next.config.js
+++ next.config.js
@@ -2,9 +2,27 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
+  //i18n,
   reactStrictMode: true,
-
+  redirects: async () => {
+    return [
+      {
+        source: '/en',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/jp',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/zh',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
   webpack(config, { isServer, dev }) {
     config.experiments = {
       asyncWebAssembly: true,
